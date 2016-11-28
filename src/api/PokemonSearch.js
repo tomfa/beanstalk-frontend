d9163be5f67eaf3c320ff426afd8115123925289
Tomas Fagerbekk <tomas@webutvikling.org>
2016-11-28 22:18:52 +0100

    Add simple cache

--- src/api/PokemonSearch.js
+++ src/api/PokemonSearch.js
@@ -8,9 +8,15 @@ function getJson(response) {
   return response.json();
 }
 
+let lolCache = {};
+
 export default function pokemonSearch(name, callback) {
-  fetch(`http://pokeapi.co/api/v2/pokemon/${name}/`)
-    .then(getJson)
-    .then(callback)
-    .catch(logError);
+  if (!lolCache.hasOwnProperty(name)) {
+    lolCache[name] = fetch(`http://localhost:5000/api/pokemon/${name}/`)
+      .then(getJson)
+      .then(data => {lolCache[name] = data; callback(data); return data })
+      .catch(logError);
+  } else {
+    callback(lolCache[name]);
+  }
 }
